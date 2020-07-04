93289bee6f9e2271092c841f17d470a7dd29c433
higherhuang <240580270@qq.com>
2020-07-04 20:02:36 +0800

    增加messagebus测试工程

---
+++ MessageBusTest/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include "MessageBus.hpp"
+
+MessageBus msgBus;
+
+//
+int GlobalFunc_int_int(int a)
+{
+	std::cout << __FUNCTION__ << " called." << std::endl;
+
+	return a;
+}
+void RegisterGlobalFunc()
+{
+	std::cout << "向消息总线注册全局函数，GlobalFunc_int_int" << std::endl;
+	msgBus.Attach(&GlobalFunc_int_int, "GlobalFunc_int_int");
+}
+
+
+
+int main()
+{
+	RegisterGlobalFunc();
+
+	msgBus.SendReq<int, int>(1, "GlobalFunc_int_int");
+
+
+
+	return 0;
+}
